9e9dbf183da6b87fe50c98085c0d8c6941da167d
jaeyeun95 <jaeyeun95@gmail.com>
2021-01-28 23:04:05 +0900

    2021-01-28 수정

--- src/main/java/com/ict/ajax1/controller/TestController.java
+++ src/main/java/com/ict/ajax1/controller/TestController.java
@@ -1,5 +1,35 @@
 package com.ict.ajax1.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class TestController {
 
+	/**
+	 * 1. ServletOutputStream을 이용한 방식으로 응답데이터 출력
+	 * @param name
+	 * @param age
+	 * @throws IOException 
+	 */
+	@RequestMapping("test1.do")
+	public void test1Method(@RequestParam("name") String name,
+							@RequestParam("age") int age,
+							HttpServletResponse response) throws IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		if(name.equals("신사임당") && age == 47) {
+			out.print("ok");
+		}else {
+			out.print("fail");
+		}
+		
+	}	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -9,6 +9,32 @@
 <body>
 	<h1 align="center">Spring에서의 AJAX 사용 테스트 페이지</h1>
 	
+	<ol>
+		<li>
+			서버쪽 컨트롤러 값 보내기
+			<button id="btn1">테스트</button>
+			<div id="d1"></div>
+		</li>
+		<script>
+			$(function({
+				$("#btn1").on("click",function(){
+					$.ajax({
+						url:"test1.do",
+						type:"post",
+						data:{name:"신사임당",age:47},
+						success:function(data){
+							console.log(data);
+						}, error:function(request,status,errorDate){
+							alert("error code : " + request.status + "\n"
+									+ "message : " + reuqest.responseText
+									+ "error : " + errorData);
+						}
+					});
+				});
+			}));
+		</script>
+	</ol>
+	
 	
 </body>
 </html>
